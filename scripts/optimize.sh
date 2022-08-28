d9c836172ef51413e5a233a0d8687b6166fdf009
SebastiaanYN <sebastiaanyn@gmail.com>
2022-08-28 19:04:07 +0200

    Optimize for speed

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-wasm-opt -Oz -o ffmpeg.wasm ffmpeg.wasm
-wasm-opt -Oz -o ffprobe.wasm ffprobe.wasm
+wasm-opt -O3 -o ffmpeg.wasm ffmpeg.wasm
+wasm-opt -O3 -o ffprobe.wasm ffprobe.wasm
